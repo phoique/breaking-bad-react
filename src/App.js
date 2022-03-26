eaef98c67a15aba8c54bf351d0babd96b582a967
Enes Kahraman <official.phoique@gmail.com>
2022-03-26 15:25:30 +0300

    Package update
    + Paketler güncellendi.
    + Router dom v6 uygun şekilde güncellendi.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 // import Components
 import { Menu } from "./components";
@@ -17,17 +17,17 @@ import {
 function App() {
   return (
     <div className="container">
-      <Router>
+      <BrowserRouter>
         <Menu />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/characters" exact component={Characters} />
-          <Route path="/character/:id" exact component={CharacterDetail} />
-          <Route path="/quotes" exact component={Quotes} />
-          <Route path="/quote/:id" exact component={QuoteDetail} />
-          <Route path="*" component={NotFound} />
-        </Switch>
-      </Router>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/characters" exact element={<Characters />} />
+          <Route path="/character/:id" exact element={<CharacterDetail />} />
+          <Route path="/quotes" exact element={<Quotes />} />
+          <Route path="/quote/:id" exact element={<QuoteDetail />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
